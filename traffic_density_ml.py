92b6610b01f8d893f97921768237980855eb59da
darma09 <55343445+darma09@users.noreply.github.com>
2024-07-24 11:24:22 +0700

    Update traffic_density_ml.py

--- traffic_density_ml.py
+++ traffic_density_ml.py
@@ -6,7 +6,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-pip install --upgrade pip
 
 def load_images_from_folder(folder):
     images = []
