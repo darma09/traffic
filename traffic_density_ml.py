4b3c0bc080477f71121bb83eeb69f0c9b255dc91
darma09 <55343445+darma09@users.noreply.github.com>
2024-07-25 16:57:14 +0700

    Update traffic_density_ml.py

--- traffic_density_ml.py
+++ traffic_density_ml.py
@@ -10,23 +10,21 @@ import io
 # Function to load YOLOv5 model
 @st.cache_resource
 def load_yolo_model():
-    model = torch.hub.load('ultralytics/yolov5', yolov5s, pretrained=True)
+    model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
     return model
 
- @st.cache_resource
+@st.cache_resource
 def load_data_from_url(url):
     response = requests.get(url)
-    drive_file = io.StringIO(
-    drive_file.content.decode('utf-8'), drive_file.content)
+    drive_file = io.StringIO(response.content.decode('utf-8'))
     data = pd.read_csv(drive_file)
     return data
 
 def process_image(uploaded_file, model):
     image = Image.open(uploaded_file)
     results = model(image)
-    results render() # updates results.imgs with boxes and labels
+    results.render()  # updates results.imgs with boxes and labels
     return Image.fromarray(results.imgs[0])
-
 
 # Streamlit app
 st.title("Data Analysis and Object Detection App")
@@ -42,17 +40,17 @@ st.dataframe(data)
 
 # Step 4: File uploader for image
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-if uploaded_file is not None:
-    st.weite("Processing image...")
 
-    # Load YOLO model
+if uploaded_file is not None:
+    st.write("Processing image...")
+    
+    # Load YOLOv5 model
     model = load_yolo_model()
 
     # Step 5: Analyze the uploaded image
     result_image = process_image(uploaded_file, model)
 
     # Step 6: Display the result of image analysis
-st.image(result_image, caption='Processed Image',
-    use_column_width=True)
+    st.image(result_image, caption='Processed Image', use_column_width=True)
 else:
     st.write("Please upload an image file.")
