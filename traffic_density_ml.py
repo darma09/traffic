456459a35222812f427a231c408618eb38dd51fd
darma09 <55343445+darma09@users.noreply.github.com>
2024-07-25 11:02:47 +0700

    Update traffic_density_ml.py

--- traffic_density_ml.py
+++ traffic_density_ml.py
@@ -3,29 +3,38 @@ import pandas as pd
 import joblib
 import gdown
 import os
+import sklearn
+
+@st.cache_data
+def load_csv_data(url):
+    try:
+        data = pd.read_csv(url)
+        st.write("CSV data loaded successfully.")
+        return data
+    except Exception as e:
+        st.error(f"Error loading CSV data: {str(e)}")
+        raise e
+
+@st.cache_data
+def download_and_load_model(url, model_path):
+    try:
+        gdown.download(url, model_path, quiet=False)
+        random_forest_model = joblib.load(model_path)
+        st.write("Random Forest model loaded successfully.")
+        return random_forest_model
+    except Exception as e:
+        st.error(f"Error loading the Random Forest model: {str(e)}")
+        raise e
 
 # Load CSV data
 csv_url = 'https://raw.githubusercontent.com/darma09/traffic/main/Metro_Interstate_Traffic_Volume.csv'
-try:
-    data = pd.read_csv(csv_url)
-    st.write("CSV data loaded successfully.")
-    st.dataframe(data)  # Display the loaded CSV file in Streamlit
-except Exception as e:
-    st.error(f"Error loading CSV data: {str(e)}")
-    raise e
+data = load_csv_data(csv_url)
+st.dataframe(data)  # Display the loaded CSV file in Streamlit
 
-# Download the Random Forest model from Google Drive
+# Download and load the Random Forest model from Google Drive
 url = 'https://drive.google.com/uc?id=1l5PvNkp3Lq8O9U41UvuW4MfaMCLyo57W'
 model_path = 'random_forest_model.pkl'
-try:
-    gdown.download(url, model_path, quiet=False)
-    # Ensure scikit-learn compatibility
-    os.system("pip install scikit-learn==1.0.2")
-    random_forest_model = joblib.load(model_path)
-    st.write("Random Forest model loaded successfully.")
-except Exception as e:
-    st.error(f"Error loading the Random Forest model: {str(e)}")
-    raise e
+random_forest_model = download_and_load_model(url, model_path)
 
 # Display information about the Random Forest model
 st.write(random_forest_model)
